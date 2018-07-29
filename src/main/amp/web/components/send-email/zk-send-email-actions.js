4e7e3b1e91c8bee4c17ce31562c1a751ac06d41a
Cesar Capillas <cesar@zylk.net>
2018-07-30 00:46:50 +0200

    Fix download url

--- src/main/amp/web/components/send-email/zk-send-email-actions.js
+++ src/main/amp/web/components/send-email/zk-send-email-actions.js
@@ -7,8 +7,13 @@
                 if(window.location.protocol.indexOf("https") != -1){
                   myprotocol = myprotocol+"s";
                 }
-          	var mymail = "mailto:?subject=Link%20from%20Alfresco&body="+myprotocol+"://"+window.location.host+"/alfresco"+file.webdavUrl+"";
-        	window.location = mymail;
+                //var downloadURL = myprotocol+"://"+window.location.host+"/alfresco"+file.webdavUrl;
+                var aux         = file.webdavUrl.split("/");
+                var naux        = aux.length;
+                var filename    = aux[naux - 1];
+                var downloadURL = myprotocol+"://"+window.location.host+"/share/proxy/alfresco/api/node/content/"+file.nodeRef.replace(":/", "")+'/'+filename;
+                var mymail      = "mailto:?subject=Link%20from%20Alfresco&body="+downloadURL;
+                window.location = mymail;
         }
     });
 })();
